81c6c17510d0dd3affc7f05f06e0c6b2369cf12b
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2020-09-07 09:11:14 +0700

    change into greetings

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
 	public static void main(String[] a) {
-		System.out.println("Hello World!");
+		System.out.println("Selamat Pagi");
+		System.out.println("Apakah anda sudah Sarapan?");
 	}
 }
